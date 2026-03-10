0766a2ef4e138ca5a6296213f39a98ca493ab833
Robert D. Smith <mirmacconsulting@gmail.com>
2026-03-10 01:38:08 -0400

    Externalize stripe for Next build

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverExternalPackages: ["stripe"],
-  },
+  serverExternalPackages: ["stripe"],
 };
 
 export default nextConfig;
